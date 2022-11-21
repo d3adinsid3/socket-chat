a37f9e913d98fc53512f8ba8a35ea14eb19216d9
anerix <anerix@disroot.org>
2022-11-21 07:49:17 +0300

    feat: base client layout

---
+++ client/src/components/Chat/Chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './chat.scss';
+
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      chat
+    </div>
+  )
+}
+
+export default Chat;
---
+++ client/src/components/List/List.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './list.scss';
+
+
+const List = () => {
+  return (
+    <div className="list">
+      list
+    </div>
+  )
+}
+
+export default List;
